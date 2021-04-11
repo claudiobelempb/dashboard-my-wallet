883d9a155dac159fbbfb7f040093fc4f8b20475b
claudiobelempb <claudio.c.lima@hotmail.com>
2021-04-11 18:30:41 -0300

    Criando o header principal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,26 @@
+import React, {useMemo} from "react"
 import { HeaderContainer } from './styles';
 
-import { Brand } from '../Brand';
-
+import emojis from "../../utils/emojis";
 // import { Button } from '../Button';
 
 export function Header() {
+
+  const emoji = useMemo(() => {
+
+    const indice = Math.floor(Math.random() * emojis.length);
+
+    return emojis[indice];
+
+  }, []);
+
   return (
-    <HeaderContainer garea={"MH"}>
-      <Brand/>
+    <HeaderContainer jcontent={"space-between"} garea={"MH"}>
+      <h1>Toggle</h1>
+      <div>
+        <h3>Olá, {emoji}</h3> 
+        <span>Cláudio Cardoso</span>
+      </div>
     </HeaderContainer>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,17 +3,30 @@ import styled from  'styled-components';
 interface IHeaderProps {
   flexDirection?: string;
   garea?:string;
+  jcontent?: string;
 }
 
 export const HeaderContainer = styled.header<IHeaderProps>`
   grid-area: ${props => props.garea ? props.garea : "MH"};
   display: flex;
-  justify-content: center;
+  justify-content: ${props => props.jcontent ? props.jcontent : "center"};
   align-items:center;
   width: 100%;
   background: ${props => props.theme.colors.dark_light};
   color: ${props => props.theme.colors.white};
-  padding: 0.25rem;
+  border-bottom: 1px solid ${props => props.theme.colors.white};
+  padding: 1rem;
+
+  & div > h3 {
+    font-size: 28px;
+    font-weight: 700;
+    line-height: 32px
+  }
+
+  & div > span {
+    font-size: 16px;
+    line-height: 18px;
+  }
 `;
 /*
 export const Content = styled.div`
